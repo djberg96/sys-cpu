f5d2e2dc44a6e41649792b98653102659e164d7a
Daniel Berger <djberg96@gmail.com>
2011-12-11 11:53:20 -0700

    Added initial version of architecture method.

--- lib/unix/sys/cpu.rb
+++ lib/unix/sys/cpu.rb
@@ -8,13 +8,24 @@ module Sys
 
     CTL_HW     = 6 # Generic hardware/cpu
 
-    HW_MACHINE  = 1  # Machine class
-    HW_MODEL    = 2  # Specific machine model
-    HW_NCPU     = 3  # Number of CPU's
-    HW_CPU_FREQ = 15 # CPU frequency
+    HW_MACHINE      = 1  # Machine class
+    HW_MODEL        = 2  # Specific machine model
+    HW_NCPU         = 3  # Number of CPU's
+    HW_MACHINE_ARCH = 12 # CPU frequency
+    HW_CPU_FREQ     = 15 # CPU frequency
 
     attach_function :sysctl, [:pointer, :uint, :pointer, :pointer, :pointer, :size_t], :int
     private_class_method :sysctl
+
+    def self.architecture
+      buf  = 0.chr * 64
+      mib  = FFI::MemoryPointer.new(:int, 2).write_array_of_int([CTL_HW, HW_MACHINE_ARCH])
+      size = FFI::MemoryPointer.new(:long, 1).write_int(buf.size)
+
+      sysctl(mib, 2, buf, size, nil, 0)
+
+      buf.strip
+    end
 
     def self.num_cpu
       buf  = 0.chr * 4

6e18f8c5a01c60d2fbb0135fd785bfbcd3fc43f9
Daniel Berger <djberg96@gmail.com>
2011-12-11 11:26:54 -0700

    Added machine method.

--- lib/unix/sys/cpu.rb
+++ lib/unix/sys/cpu.rb
@@ -1,21 +1,38 @@
 require 'ffi'
+require 'rbconfig'
 
 module Sys
   class CPU
     extend FFI::Library
     ffi_lib FFI::Library::LIBC
 
+    CTL_HW     = 6 # Generic hardware/cpu
+
+    HW_MACHINE = 1 # Machine class
+    HW_MODEL   = 2 # Specific machine model
+    HW_NCPU    = 3 # Number of cpus
+
     attach_function :sysctl, [:pointer, :uint, :pointer, :pointer, :pointer, :size_t], :int
     private_class_method :sysctl
 
     def self.num_cpu
       buf  = 0.chr * 4
-      mib  = FFI::MemoryPointer.new(:int, 2).write_array_of_int([6, 3])
+      mib  = FFI::MemoryPointer.new(:int, 2).write_array_of_int([CTL_HW, HW_NCPU])
       size = FFI::MemoryPointer.new(:long, 1).write_int(buf.size)
 
       sysctl(mib, 2, buf, size, nil, 0)
 
       buf.strip.unpack("C").first
+    end
+
+    def self.machine
+      buf  = 0.chr * 32
+      mib  = FFI::MemoryPointer.new(:int, 2).write_array_of_int([CTL_HW, HW_MACHINE])
+      size = FFI::MemoryPointer.new(:long, 1).write_int(buf.size)
+
+      sysctl(mib, 2, buf, size, nil, 0)
+
+      buf.strip
     end
   end
 end

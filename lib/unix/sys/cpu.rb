6252027e042fe72d151f476dbbf2a3f871d9d77a
Daniel Berger <djberg96@gmail.com>
2011-12-11 11:19:38 -0700

    Added num_cpu method and sysctl ffi function.

--- lib/unix/sys/cpu.rb
+++ lib/unix/sys/cpu.rb
@@ -4,5 +4,18 @@ module Sys
   class CPU
     extend FFI::Library
     ffi_lib FFI::Library::LIBC
+
+    attach_function :sysctl, [:pointer, :uint, :pointer, :pointer, :pointer, :size_t], :int
+    private_class_method :sysctl
+
+    def self.num_cpu
+      buf  = 0.chr * 4
+      mib  = FFI::MemoryPointer.new(:int, 2).write_array_of_int([6, 3])
+      size = FFI::MemoryPointer.new(:long, 1).write_int(buf.size)
+
+      sysctl(mib, 2, buf, size, nil, 0)
+
+      buf.strip.unpack("C").first
+    end
   end
 end

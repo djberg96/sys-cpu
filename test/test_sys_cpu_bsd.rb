0b40be0c20233d88c86e2681adf587e3de8bfc9c
Daniel Berger <djberg96@gmail.com>
2011-12-13 21:43:31 -0700

    Updated bsd tests.

--- test/test_sys_cpu_bsd.rb
+++ test/test_sys_cpu_bsd.rb
@@ -13,64 +13,88 @@ require 'test_sys_cpu_version'
 include Sys
 
 class TC_Sys_CPU_BSD < Test::Unit::TestCase
-   def test_architecture
-      assert_respond_to(CPU, :architecture)
-      assert_nothing_raised{ CPU.architecture }
-      assert_kind_of(String, CPU.architecture)
-   end
+  test "architecture method basic functionality" do
+    assert_respond_to(CPU, :architecture)
+    assert_nothing_raised{ CPU.architecture }
+  end
 
-   def test_architecture_expected_errors
-      assert_raises(ArgumentError){ CPU.architecture(0) }
-   end
+  test "architecture method returns a sane value" do
+    assert_kind_of(String, CPU.architecture)
+    assert_true(CPU.architecture.size > 0)
+  end
 
-   def test_cpu_freq
-      assert_respond_to(CPU, :freq)
-      assert_nothing_raised{ CPU.freq }
-      assert_kind_of(Integer, CPU.freq)
-   end
+  test "architecture method does not accept any arguments" do
+    assert_raises(ArgumentError){ CPU.architecture(0) }
+  end
 
-   def test_cpu_freq_expected_errors
-      assert_raises(ArgumentError){ CPU.freq(0) }
-   end
+  test "freq method basic functionality" do
+    assert_respond_to(CPU, :freq)
+    assert_nothing_raised{ CPU.freq }
+  end
 
-   def test_load_avg
-      assert_respond_to(CPU, :load_avg)
-      assert_nothing_raised{ CPU.load_avg }
-      assert_kind_of(Array, CPU.load_avg)
-      assert_equal(3,CPU.load_avg.length)
-   end
+  test "freq method returns expected value" do
+    assert_kind_of(Integer, CPU.freq)
+    assert_true(CPU.freq > 0)
+  end
 
-   def test_load_avg_expected_errors
-      assert_raises(ArgumentError){ CPU.load_avg(0) }
-   end
-   
-   def test_machine
-      assert_respond_to(CPU, :machine)
-      assert_nothing_raised{ CPU.machine }
-      assert_kind_of(String, CPU.machine)
-   end
+  test "freq method does not accept any arguments" do
+    assert_raises(ArgumentError){ CPU.freq(0) }
+  end
 
-   def test_machine_expected_errors
-      assert_raises(ArgumentError){ CPU.machine(0) }
-   end
+  test "load_avg method basic functionality" do
+    assert_respond_to(CPU, :load_avg)
+    assert_nothing_raised{ CPU.load_avg }
+  end
 
-   def test_model
-      assert_respond_to(CPU, :model)
-      assert_nothing_raised{ CPU.model }
-      assert_kind_of(String, CPU.model)
-   end
+  test "load_avg returns the expected results" do
+    assert_kind_of(Array, CPU.load_avg)
+    assert_equal(3, CPU.load_avg.length)
+    assert_kind_of(Float, CPU.load_avg[0])
+  end
 
-   def test_model_expected_errors
-      assert_raises(ArgumentError){ CPU.model(0) }
-   end
+  test "load_avg does not accept any arguments" do
+    assert_raises(ArgumentError){ CPU.load_avg(0) }
+  end
 
-   def test_num_cpu
-      assert_respond_to(CPU, :num_cpu)
-      assert_nothing_raised{ CPU.num_cpu }
-      assert_kind_of(Integer, CPU.num_cpu)
-   end
+  test "machine method basic functionality" do
+    assert_respond_to(CPU, :machine)
+    assert_nothing_raised{ CPU.machine }
+  end
 
-   def test_num_cpu_expected_errors
-      assert_raises(ArgumentError){ CPU.num_cpu(0) }
-   end
+  test "machine method returns sane value" do
+    assert_kind_of(String, CPU.machine)
+    assert_true(CPU.machine.size > 0)
+  end
+
+  test "machine method does not accept any arguments" do
+    assert_raises(ArgumentError){ CPU.machine(0) }
+  end
+
+  test "model method basic functionality" do
+    assert_respond_to(CPU, :model)
+    assert_nothing_raised{ CPU.model }
+  end
+
+  test "model method returns sane value" do
+    assert_kind_of(String, CPU.model)
+    assert_true(CPU.model.length > 0)
+  end
+
+  test "model method does not accept any arguments" do
+    assert_raises(ArgumentError){ CPU.model(0) }
+  end
+
+  test "num_cpu method basic functionality" do
+    assert_respond_to(CPU, :num_cpu)
+    assert_nothing_raised{ CPU.num_cpu }
+  end
+
+  test "num_cpu method returns expected value" do
+    assert_kind_of(Integer, CPU.num_cpu)
+    assert_true(CPU.num_cpu > 0)
+  end
+
+  test "num_cpu method does not accept any arguments" do
+    assert_raises(ArgumentError){ CPU.num_cpu(0) }
+  end
 end

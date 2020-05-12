56aca36d0f91308581d31eb7b1d313c939d37340
Daniel Berger <djberg96@gmail.com>
2020-05-12 10:19:31 -0400

    Add freq, architecture, num_cpu common methods.

--- lib/sys/linux/sys/cpu.rb
+++ lib/sys/linux/sys/cpu.rb
@@ -70,6 +70,37 @@ module Sys
       array unless block_given?
     end
 
+    # Return the total number of logical CPU on the system.
+    #
+    def self.num_cpu
+      CPU_ARRAY.size
+    end
+
+    # Return the architecture of the CPU.
+    #
+    def self.architecture
+      case CPU_ARRAY.first['cpu_family']
+      when '3'
+        "x86"
+      when '6'
+        "x86_64"
+      else
+        nil
+      end
+    end
+
+    # Returns a string indicating the CPU model.
+    #
+    def self.model
+      CPU_ARRAY.first['model_name']
+    end
+
+    # Returns an integer indicating the speed of the CPU.
+    #
+    def self.freq
+      CPU_ARRAY.first['cpu_mhz'].to_f.round
+    end
+
     private
 
     # Create singleton methods for each of the attributes.

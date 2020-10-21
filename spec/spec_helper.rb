7ae3f7d0bce34b8b8beacf71c118f40238d5b48b
Daniel Berger <djberg96@gmail.com>
2020-10-21 15:37:42 -0400

    Minor tweaks to spec_helper.rb.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 RSpec.configure do |config|
-  config.filter_run_excluding(:bsd) unless RbConfig::CONFIG['host_os'] =~ /bsd|darwin|mach|osx/i
-  config.filter_run_excluding(:sunos) unless RbConfig::CONFIG['host_os'] =~ /sunos|solaris/i
-  config.filter_run_excluding(:windows) unless RbConfig::CONFIG['host_os'] =~ /mswin|win32|dos|mingw|cygwin/i
-  config.filter_run_excluding(:hpux) unless RbConfig::CONFIG['host_os'] =~ /hpux/i
-  config.filter_run_excluding(:linux) unless RbConfig::CONFIG['host_os'] =~ /linux/i
+  config.filter_run_excluding(:bsd) if RbConfig::CONFIG['host_os'] !~ /bsd|darwin|mach|osx/i
+  config.filter_run_excluding(:sunos) if RbConfig::CONFIG['host_os'] !~ /sunos|solaris/i
+  config.filter_run_excluding(:windows) if RbConfig::CONFIG['host_os'] !~ /mswin|win32|dos|mingw|cygwin/i
+  config.filter_run_excluding(:hpux) if RbConfig::CONFIG['host_os'] !~ /hpux/i
+  config.filter_run_excluding(:linux) if RbConfig::CONFIG['host_os'] !~ /linux/i
 end

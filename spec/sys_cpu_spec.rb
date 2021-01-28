bce1f4bf7bb23b1ca87f1e3114dbe4f088778193
Daniel Berger <djberg96@gmail.com>
2021-01-28 15:00:00 -0500

    Update changes and manifest files, bump versions.

--- spec/sys_cpu_spec.rb
+++ spec/sys_cpu_spec.rb
@@ -9,7 +9,7 @@ require 'rspec'
 
 RSpec.describe Sys::CPU::VERSION do
   example "version number is set to the expected value" do
-    expect(Sys::CPU::VERSION).to eq('1.0.2')
+    expect(Sys::CPU::VERSION).to eq('1.0.3')
   end
 
   example "version number is frozen" do

c0906e8d2a756c3dd9f10eacb9f562b98032571c
Daniel Berger <djberg96@gmail.com>
2022-04-13 13:35:30 -0400

    Switch to shared examples, update Linux specs.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'rspec'
+require 'sys_cpu_spec'
 
 RSpec.configure do |config|
   config.filter_run_excluding(:bsd) if RbConfig::CONFIG['host_os'] !~ /bsd|darwin|mach|osx/i
--- spec/sys_cpu_linux_spec.rb
+++ spec/sys_cpu_linux_spec.rb
@@ -10,6 +10,8 @@ require 'sys/cpu'
 require 'spec_helper'
 
 RSpec.describe Sys::CPU, :linux => true do
+  include_examples 'Sys::CPU Common'
+
   example 'dynamic methods are defined as expected' do
     expect do
       described_class.processors do |cs|

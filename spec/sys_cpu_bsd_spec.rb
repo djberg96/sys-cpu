1d7890b105a71a2256d07f52f3913717e8e47ef6
Daniel Berger <daniel.berger@covermymeds.com>
2022-08-12 15:33:08 -0400

    Make ffi related constants private.

--- spec/sys_cpu_bsd_spec.rb
+++ spec/sys_cpu_bsd_spec.rb
@@ -93,4 +93,21 @@ RSpec.describe Sys::CPU, :bsd => true do
   example 'num_cpu method does not accept any arguments' do
     expect{ described_class.num_cpu(0) }.to raise_error(ArgumentError)
   end
+
+  context "ffi methods and constants are private" do
+    example "ffi constants are private" do
+      constants = described_class.constants
+      expect(constants).not_to include(:CTL_HW)
+      expect(constants).not_to include(:CPU_TYPE_X86)
+      expect(constants).not_to include(:CPU_TYPE_X86_64)
+      expect(constants).not_to include(:HW_MACHINE)
+      expect(constants).not_to include(:ClockInfo)
+    end
+
+    example "ffi methods are private" do
+      methods = described_class.methods(false)
+      expect(methods).not_to include(:sysctl)
+      expect(methods).not_to include(:sysctlbyname)
+    end
+  end
 end

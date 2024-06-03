2513d3ad9dbcf90bd8e7c986596ca2b52b8ee052
Daniel Berger <78529+djberg96@users.noreply.github.com>
2024-06-03 18:42:23 -0400

    Add dragonfly to host os checks, make constants private.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,8 +5,7 @@ require 'sys_cpu_shared'
 
 RSpec.configure do |config|
   config.include_context(Sys::CPU)
-  config.filter_run_excluding(:bsd) if RbConfig::CONFIG['host_os'] !~ /bsd|darwin|mach|osx/i
-  config.filter_run_excluding(:sunos) if RbConfig::CONFIG['host_os'] !~ /sunos|solaris/i
+  config.filter_run_excluding(:bsd) if RbConfig::CONFIG['host_os'] !~ /bsd|darwin|mach|osx|dragonfly/i
   config.filter_run_excluding(:windows) if RbConfig::CONFIG['host_os'] !~ /mswin|win32|dos|mingw|cygwin/i
   config.filter_run_excluding(:hpux) if RbConfig::CONFIG['host_os'] !~ /hpux/i
   config.filter_run_excluding(:linux) if RbConfig::CONFIG['host_os'] !~ /linux/i
